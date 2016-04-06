49536c549d1c75c0a835636476598217aa88b0f2
bobros <vvvwvvm@gmail.com>
2016-04-06 13:02:11 +0900

    use ejs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
 
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-console.log(__dirname);
+
+app.get('/', function(req, res){
+  res.render('my_first_ejs');
+});
 
 app.listen(3000, function(){
   console.log('Server On!');
