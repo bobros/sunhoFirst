bdf56700fbbb22d05d42adf2275ce70d77e4bb00
bobros <vvvwvvm@gmail.com>
2016-04-06 16:45:11 +0900

    use MONGGO_DB env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb://test:testtest@ds015780.mlab.com:15780/my_first_md");
+mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function(){
   console.log("DB connected!");
@@ -12,16 +12,35 @@ db.on("error", function(err){
   console.log("DB ERROR : ", err);
 });
 
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+
+var Data = mongoose.model('data', dataSchema);
+Data.findOne({name:"myData"}, function(err, data){
+  if(err) return console.log("Data ERROR : ", err);
+  if(!data){
+    Data.create({name:"myData", count:0}, function(err, data){
+      if(err) return console.log("Data ERROR : ", err);
+      console.log("Counter initialized : ", data);
+    });
+  }
+});
+
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-var data={count:0};
 app.get('/', function(req, res){
-  //console.log(req);
-  //console.log(res);
-  console.log("count : " + data.count);
-  data.count++;
-  res.render('my_first_ejs', data);
+  Data.findOne({name:"myData"}, function(err, data){
+    if(err) return console.log("Data ERROR : ", err);
+    data.count++;
+    data.save(function(err){
+      if(err) return console.log("Data ERROR : ", err);
+      res.render('my_first_ejs', data);
+    });
+  });
+
 });
 app.get('/reset', function(req, res){
   data.count=0;
